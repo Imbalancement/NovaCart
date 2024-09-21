a9aaa02cec3c28ef7a00cf0c616f020fe5845f83
Imbalancement <138077219+Imbalancement@users.noreply.github.com>
2024-09-20 21:09:21 -0400

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,24 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+      },
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase the chunk size limit (in KB)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Separate node_modules into a vendor chunk
+          }
+        },
+      },
+    },
+  },
 });
